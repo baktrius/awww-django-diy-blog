6b09268ef0e3dfd1364da4dc2fcb3b9606518223
Krzysztof Rogowski <krzysztor99@gmail.com>
2023-04-18 21:17:23 +0200

    Detail+comment view for blogs (task 11)

--- blog/views.py
+++ blog/views.py
@@ -19,8 +19,6 @@ def index(request):
     )
     
 
-# TODO: Create Views for BlogDetailView
-
 class BlogListView(generic.ListView):
     """
     Generic class-based view for a list of all blogs.
@@ -28,10 +26,53 @@ class BlogListView(generic.ListView):
     model = Blog
     paginate_by = 5
 
-
 class BloggerListView(generic.ListView):
     """
     Generic class-based view for a list of bloggers.
     """
     model = BlogAuthor
     paginate_by = 5
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView
+from django.urls import reverse
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
+
+
+class BlogDetailView(CreateView):
+    """
+    Form for adding a blog comment. Requires login. 
+    """
+    model = BlogComment
+    fields = ['description',]
+    template_name = 'blog/blog_detail.html'
+
+    def get_context_data(self, **kwargs):
+        """
+        Add associated blog to form template so can display its title in HTML.
+        """
+        # Call the base implementation first to get a context
+        context = super(BlogDetailView, self).get_context_data(**kwargs)
+        # Get the blog from id and add it to the context
+        context['blog'] = get_object_or_404(Blog, pk = self.kwargs['pk'])
+        return context
+        
+    def form_valid(self, form):
+        """
+        Add author and associated blog to form data before setting it as valid (so it is saved to model)
+        """
+        if not self.request.user.is_authenticated:
+            return HttpResponse('Unauthorized', status=401)
+        #Add logged-in user as author of comment
+        form.instance.author = self.request.user
+        #Associate comment with blog based on passed id
+        form.instance.blog=get_object_or_404(Blog, pk = self.kwargs['pk'])
+        # Call super-class form validation behaviour
+        return super(BlogDetailView, self).form_valid(form)
+
+    def get_success_url(self): 
+        """
+        After posting comment return to associated blog.
+        """
+        return reverse('blog-detail', kwargs={'pk': self.kwargs['pk'],})
